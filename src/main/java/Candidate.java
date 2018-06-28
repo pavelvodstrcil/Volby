2207758cec5648128c2394567a082c51e2faf7f6
Antaury <adrian.czarnomski@seznam.cz>
2018-06-28 09:10:33 +0200

    Děcka, upravil jsem třídu Candidate, dále jsem přidal třídu ResultWindow

--- src/main/java/Candidate.java
+++ src/main/java/Candidate.java
@@ -1,16 +1,33 @@
-
 /**
  *
  * @author User
  */
    public class Candidate{
         private long id;
+        private String firstName;
+        private String lastName;
+        private int countVotes;
         
         public Candidate(long id){
             this.id = id;
         }
         
+        public Candidate(long id, String firstName, String lastName, int countVotes){
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.countVotes = countVotes;
+        }
+        
         public long getID(){
             return this.id;
+        }
+        
+        public String getName(){
+            return firstName + " " + lastName;
+        }
+        
+        public int getCount(){
+            return countVotes;
         }
 }
