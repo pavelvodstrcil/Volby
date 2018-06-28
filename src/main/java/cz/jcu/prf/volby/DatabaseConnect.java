7aac47911f17424b0389ce2be1dbb4feb150d6aa
Pavel Vodstrcil <pavelvodstrcil@gmail.com>
2018-06-28 12:37:29 +0200

    pom: pridana deendence Pokus o pripojeni k databazi - nefunguje.

--- src/main/java/cz/jcu/prf/volby/DatabaseConnect.java
+++ src/main/java/cz/jcu/prf/volby/DatabaseConnect.java
@@ -10,15 +10,18 @@ public class DatabaseConnect {
     private Statement st;
     private ResultSet rs;
 
-    
-    public static void main(String[] args) throws SQLException {
-     DatabaseConnect pokus = DatabaseConnect(); 
+    public static void main(String[] args) {
+         DatabaseConnect pokus = new DatabaseConnect(); 
     }
+    
+    
     public DatabaseConnect() {
         try {
+            Class.forName("com.mysql.jdbc.Driver");
            con = DriverManager.getConnection("jbdc:mysql://db4free.net:3306/volby_2018", "babis_2018","capihnizdo");
+      
         } catch (Exception ex) {
-
+            System.out.println("nejde to "+ex);
         }
 
     }
