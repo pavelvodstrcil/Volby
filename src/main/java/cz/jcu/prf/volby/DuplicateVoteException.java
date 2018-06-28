fc189841d96b69a6891a26b2fd41bd9b0d7a14b8
Pavel Vodstrcil <pavelvodstrcil@gmail.com>
2018-06-28 11:25:49 +0200

    kostruktory u vyjimky

--- src/main/java/cz/jcu/prf/volby/DuplicateVoteException.java
+++ src/main/java/cz/jcu/prf/volby/DuplicateVoteException.java
@@ -5,11 +5,24 @@ package cz.jcu.prf.volby;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author User
  */
-public class DuplicateVoteException extends Exception{
-    
+public class DuplicateVoteException extends Exception {
+
+    public DuplicateVoteException() {
+    }
+
+    public DuplicateVoteException(String message) {
+        super(message);
+    }
+
+    public DuplicateVoteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateVoteException(Throwable cause) {
+        super(cause);
+    }
 }
