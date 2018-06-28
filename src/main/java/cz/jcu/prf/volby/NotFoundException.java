8d7e04dc271488c993f395fe0f5fc5703be4e89d
Pavel Vodstrcil <pavelvodstrcil@gmail.com>
2018-06-28 11:34:58 +0200

    pridane konstruktory.

--- src/main/java/cz/jcu/prf/volby/NotFoundException.java
+++ src/main/java/cz/jcu/prf/volby/NotFoundException.java
@@ -12,4 +12,18 @@ package cz.jcu.prf.volby;
  */
 public class NotFoundException extends Exception{
     
+     public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
