6452166d2fcf71f24eff9704ac6bdbd74d7fee8c
Pavel Vodstrcil <pavelvodstrcil@gmail.com>
2018-06-28 09:03:07 +0200

    pridano LoggedOne

---
+++ src/main/java/LoggedOne.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class LoggedOne {
+
+    private String hashOP;
+    private String jmeno;
+    private String prijmeni;
+
+    public String getHashOP() {
+        return hashOP;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public LoggedOne(String[] array) {
+
+        this.hashOP = array[0];
+        this.jmeno = array[1];
+        this.prijmeni = array[2];
+    }
+
+}
